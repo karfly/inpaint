a7521b4f6b38124450e261a139fcc52fb247e61c
Yuriy Gorishniy <strausmg@gmail.com>
2018-05-12 16:27:28 +0300

    add missing frontend files; change .gitignore; style fixes in python code

--- app/app.py
+++ app/app.py
@@ -1,34 +1,41 @@
 import base64
+import io
 import os
 import random
-from io import BytesIO
-from os.path import join as pj
 
 from PIL import Image, ImageOps
 from flask import Flask, render_template, send_file, request
 
+
+STATIC_DIR = 'static/images'
+
+
 app = Flask(__name__)
+
+
+def extract_image(request, field_name):
+    return request.form[field_name][22:]
 
 
 @app.route('/apply', methods=['POST'])
 def apply():
-    encoded_image = request.form['image'][22:]
-    encoded_mask = request.form['mask'][22:]
-    image = Image.open(BytesIO(base64.b64decode(encoded_image)))
-    mask = Image.open(BytesIO(base64.b64decode(encoded_mask)))
+    encoded_image = extract_image(request, 'image')
+    encoded_mask = extract_image(request, 'mask')
+    image = Image.open(io.BytesIO(base64.b64decode(encoded_image)))
+    mask = Image.open(io.BytesIO(base64.b64decode(encoded_mask)))
     mask = mask.resize(image.size)
     image.paste(mask, (0, 0), mask)
 
-    buffered = BytesIO()
+    buffered = io.BytesIO()
     image.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue())
 
 
 @app.route('/pick_random')
 def pick_random():
-    images = os.listdir('./static/images')
+    images = os.listdir(STATIC_DIR)
     img_name = random.choice(images)
-    return send_file(pj('./static/images', img_name))
+    return send_file(os.path.join(STATIC_DIR, img_name))
 
 
 @app.route('/')
